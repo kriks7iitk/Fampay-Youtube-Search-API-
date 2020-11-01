58bb0eadcf1969b4262ebfc15b8710525294379a
kriks <kriks.iitk@gmail.com>
2020-11-01 08:47:51 +0530

    Add functionality to store video data for given querry

--- env/YoutubeAPI/fetching_data/views.py
+++ env/YoutubeAPI/fetching_data/views.py
@@ -1,18 +1,49 @@
 from django.shortcuts import render
 import requests
 from django.conf import settings
+from isodate import parse_duration
 
 # Create your views here.
 def index(request):
-    youtube_url = 'https://www.googleapis.com/youtube/v3/search'
+    if(request.method == 'GET'):
+        youtube_url = 'https://www.googleapis.com/youtube/v3/search'
+        video_url = 'https://www.googleapis.com/youtube/v3/videos'
 
-    params = {
-        'part' : 'snippet',
-        'q' : 'songs',
-        'key': settings.YOUTUBE_DATA_API_KEY,
-        'maxResults': 9,
-        'order' : 'date'
-    }
-    r = requests.get(youtube_url,params = params)
-    print(r.text)
-    return render(request,'dashboard/index.html')
+        search_params = {
+            'part' : 'snippet',
+            'q' : 'songs',
+            'key': settings.YOUTUBE_DATA_API_KEY,
+            'maxResults': 9,
+            'order' : 'date'
+        }
+        video_ids = []
+        r = requests.get(youtube_url,params = search_params)
+        results = r.json()['items']
+
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+
+        video_params = {
+            'key' : settings.YOUTUBE_DATA_API_KEY,
+            'part' : 'snippet,contentDetails',
+            'id' : ','.join(video_ids)
+        }
+        r = requests.get(video_url,params = video_params)
+        # print(r.text);
+        results = r.json()['items']
+
+        videos = []
+
+        for result in results:
+            video_data = {
+                'title' : result['snippet']['title'],
+                'id': result['id'],
+                'duration' : parse_duration(result['contentDetails']['duration']).total_seconds()//60,
+                'thumbnail' : result['snippet']['thumbnails']['high']['url'],
+                'publishtime' : result['snippet']['publishedAt'],
+                'description' : result['snippet']['description']
+                }
+            videos.append(video_data)
+
+
+        return render(request,'dashboard/index.html')
